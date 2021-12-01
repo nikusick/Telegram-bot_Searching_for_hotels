c523c353f21647b71008e67fdce933373cc8b580
Вероника Сибова <nika.sibova.03@inbox.ru>
2021-12-01 20:46:47 +0300

    hello world

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import telebot
 
 bot = telebot.TeleBot('2144775858:AAH2coKvBIOUH2rQxDawqVTM3i3pVwH6fPE')
+
+
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text:
+    if message.text in ["Привет", '/hello-world']:
         bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+
 
 bot.polling(none_stop=True, interval=0)

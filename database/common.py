c354278d2bc52afc1e664f77456499914c53b9b3
veronika_sibova <nika.sibova.03@inbox.ru>
2023-07-10 11:48:54 +0300

    doc to db

--- database/common.py
+++ database/common.py
@@ -19,14 +19,22 @@ create table queries (
 
 
 def generate_db() -> NoReturn:
-
+    """
+    Создание базы данных
+    """
     with sqlite3.connect("telebot.db") as conn:
         cursor = conn.cursor()
         cursor.executescript(CREATE_TABLES)
         conn.commit()
 
 
-def add_user(telegram_id: str, conn: Connection) -> str:
+def get_user_id(telegram_id: str, conn: Connection) -> str:
+    """
+    Получение id пользователя по его telegram_id. Если пользователя нет в базе данных, его добавление.
+    @param telegram_id: Id пользователя в Telegram
+    @param conn: SQLite объект подключения к базе данных.
+    @return: id пользователя
+    """
     cursor = conn.cursor()
     cursor.execute("select user_id from users "
                    "where telegram_user_id = ?", (telegram_id,))
@@ -35,14 +43,23 @@ def add_user(telegram_id: str, conn: Connection) -> str:
         cursor.execute("insert into users(telegram_user_id) "
                        "values(?)", (telegram_id,))
         conn.commit()
-        return add_user(telegram_id, conn)
+        return get_user_id(telegram_id, conn)
     else:
         return data[0][0]
 
 
 def add_query(data: Dict[str, str]) -> NoReturn:
+    """
+    Добавление запроса в базу данных
+    ================================
+    @param data: Информация о запросе:
+    - Введенная команда
+    - Город
+    - Дата въезда в отель
+    - Дата выезда из отеля
+    """
     with sqlite3.connect("telebot.db") as conn:
-        user_id = add_user(data.get('user_id'), conn)
+        user_id = get_user_id(data.get('user_id'), conn)
         cursor = conn.cursor()
         cursor.execute("insert into queries(query_text, user_id) values(?, ?)",
                        (f'Команда: {data.get("command")}\n'
@@ -53,6 +70,12 @@ def add_query(data: Dict[str, str]) -> NoReturn:
 
 
 def get_history(user_id: str, limit: int) -> List[str]:
+    """
+    Получение истории запросов
+    @param user_id: Telegram-id пользователя
+    @param limit: Число запросов, выведенных пользователю
+    @return: Список запросов в количестве limit пользователя с id user_id
+    """
     with sqlite3.connect("telebot.db") as conn:
         cursor = conn.cursor()
         cursor.execute("select query_text from queries "
